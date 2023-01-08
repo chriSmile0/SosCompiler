8ea0f65ef9936c308b29364279175078c050fbcf
SPITZ CHRISTOPHER <christopher.spitz@etu.unistra.fr>
2023-01-08 18:12:42 +0100

    tout les cas de comparaison dans une seule fonctione mips (sauf le cas différents :(

--- test_mips.s
+++ test_mips.s
@@ -1,6 +1,6 @@
         .data
-_0:     .asciiz "23"
-_1:     .asciiz "22"
+_0:     .asciiz "55"
+_1:     .asciiz "55"
         .text
 strlen:
         li $t2, 0
@@ -14,54 +14,6 @@ exit_fnclen:
         move $a0, $t2
         jr $ra
 
-compare_str:
-        move $t0 $a2
-        move $t1 $a3
-        bgt $t0,$t1 bigger
-        beq $t0,$t1 not_equal
-        li $t1 , 0
-        loop_cmp:
-        lb $t2 , ($a0)
-        lb $t3 , ($a1)
-        beqz $t2 , end_cmp
-        move $t4 , $t2
-        move $t5 , $t3
-        addi $t4, $t4, -48
-        addi $t5, $t5, -48
-        move $t6 , $a0
-        bgt $t4,$t5 little
-        li $v0 11
-        move $a0,$t2
-        syscall
-        move $a0 , $t6
-        addi $a0, $a0 , 1
-        addi $a1, $a1 , 1
-        j loop_cmp
-n_bigger:
-        li $t0 , 0
-        move $a0 $t0 
-        jr $ra 
-n_little: 
-        li $t0 , 0
-        move $a0 $t0
-        jr $ra 
-bigger:
-        li $t0 , 8
-        move $a0 $t0 
-        jr $ra 
-little: 
-        blt $t4 , $t5 bigger
-        li $t0 , 9 
-        move $a0 $t0 
-        jr $ra 
-not_equal:
-        li $t0 ,0
-        move $a0 $t0
-        jr $ra
-end_cmp:
-        li $t0 , 1
-        move $a0 $t0
-        jr $ra
 
 print_int:
     li $v0 1
@@ -69,19 +21,74 @@ print_int:
     jr $ra
 
 
+
+
+compare_strvdeux:
+        li $t1 , 0
+        la $v1 , ($a0)
+        loop_cmp:
+        lb $t2 , ($a0)
+        lb $t3 , ($a1)
+		beqz $t2 , end_cmpv
+        beqz $t3 , end_cmpvf
+        move $t4 , $t2
+        move $t5 , $t3
+        addi $t4, $t4, -48
+        addi $t5, $t5, -48
+        move $t6 , $a0
+        bne $t4,$t5 not_equalv
+        li $v0 11
+        move $a0,$t2
+        move $a0 , $t6
+        addi $a0, $a0 , 1
+        addi $a1, $a1 , 1
+        j loop_cmp
+not_equalv:
+        li $t4 ,0
+        beq $a2 ,2 true_little
+        beq $a2 ,3 egal_little
+		move $a0 $t4
+		jr $ra
+true_little:
+    	addi $a0 $zero 2
+		jr $ra 
+egal_little:
+        addi $a0 $zero 3
+		jr $ra
+not_equalvf:
+        li $t4 ,0
+        beq $a2 ,4 true_bigger
+        beq $a2 ,5 egal_bigger
+		move $a0 $t4
+		jr $ra
+true_bigger:
+    	addi $a0 $zero 4
+        jr $ra
+egal_bigger:
+        addi $a0 $zero 5
+		jr $ra
+end_cmpv:
+        li $t4 , 1
+        move $t5 , $a2
+        lb $t3 , ($a1)
+        la $v1 ($a1)
+        bnez $t3 not_equalv
+        move $a0 , $t4
+        jr $ra
+end_cmpvf:
+        li $t4 , 1
+        move $t5 , $a2
+        lb $t3 , ($a0)
+        la $v1 ($a0)
+        bnez $t3 not_equalvf
+		move $a0 , $t4
+        jr $ra
+
 main:
 la $a0 _0
-jal strlen
-move $t1 $a0
-la $a0 _1
-jal strlen
-move $t2 $a0
-la $a0 _0
 la $a1 _1
-move $a2 $t1
-move $a3 $t2
-jal compare_str
+li $a2 1
+jal compare_strvdeux
 jal print_int
-move $t1 $a0
 li $v0 10
 syscall
